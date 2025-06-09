fc5a9bae2f05bfcd6cd3c17eaff3639e556567ee
slizzy101 <124838998+slizzy101@users.noreply.github.com>
2025-06-09 14:34:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // Model URL from Teachable Machine
 //**************************************************
 //* as before, paste your lnk below
-let URL = "https://teachablemachine.withgoogle.com/models/3fKzLlseb/";
+let URL = "https://teachablemachine.withgoogle.com/models/7sfjkp4S_/";
 
 
 
@@ -231,7 +231,7 @@ async function playInstructionVideo() {
     if (model) {
         processFrame();
     } else {
-        console.log("https://teachablemachine.withgoogle.com/models/3fKzLlseb/");
+        console.log("https://teachablemachine.withgoogle.com/models/7sfjkp4S_/");
     }
 }
 
